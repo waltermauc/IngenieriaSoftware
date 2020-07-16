08846fc50124aeac90826db29ff7cdbed17e5d3d
waltermauc <56562004+waltermauc@users.noreply.github.com>
2020-07-16 07:28:14 -0500

    Update ControladorGrupo.java

--- ProyectoFinal/src/main/java/ec/ups/edu/Controlador/ControladorGrupo.java
+++ ProyectoFinal/src/main/java/ec/ups/edu/Controlador/ControladorGrupo.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package ec.ups.edu.Controlador;
+
 import ec.ups.edu.Modelo.Asignatura;
 import ec.ups.edu.Modelo.Docente;
 import ec.ups.edu.Modelo.EspacioFisico;
@@ -21,49 +22,51 @@ import java.sql.ResultSet;
  */
 public class ControladorGrupo {
 
-     private Conexion c ;
-     public ControladorGrupo (Conexion c) {
-         this.c =c;
-     }
+    private Conexion c;
 
-    public  String crearGrupo(Grupo grupo,ControladorAsignatura controlAsig, ControladorDocente controlDocen, ControladorEspacioFisico controlEspacio, ControladorNivelAsignatura nivel){
-         String res = "";
+    public ControladorGrupo(Conexion c) {
+        this.c = c;
+    }
+
+    public String crearGrupo(Grupo grupo, ControladorAsignatura controlAsig, ControladorDocente controlDocen, ControladorEspacioFisico controlEspacio, ControladorNivelAsignatura nivel) {
+        String res = "";
 
         try {
-            
-             String sqlEst = "INSERT INTO GRUPO"
+
+            String sqlEst = "INSERT INTO GRUPO"
                     + "(GRUPO_ID, GRUPO_ASIGNTURA, GRUPO_ESPACIOFISICO, GRUPO_DOCENTE) "
                     + "VALUES (?,?,?,?)";
             PreparedStatement consulta = c.conectado().prepareStatement(sqlEst);
             ResultSet resultado = consulta.executeQuery();
-           
-         while (resultado.next()) {
-             grupo.setCodigoGrupo(resultado.getInt("GRUPO_ID".trim()));
-             int codigoAsignatura = resultado.getInt("GRUPO_ASIGNTURA".trim());
-             Asignatura asig = controlAsig.buscarAsignatura(codigoAsignatura,nivel);
-             grupo.setC(asig);
-             int codigoEspacio = resultado.getInt("GRUPO_ESPACIOFISICO".trim());
-             EspacioFisico esp = controlEspacio.buscaEspacioFisico(codigoEspacio);
-             grupo.setCodigoEspacioFisico(esp);
-             int codigoDoccente = resultado.getInt("GRUPO_DOCENTE".trim());
-             Docente docne = controlDocen.buscarEsCodigo(codigoEspacio);
-             grupo.setDocenteCodigo(docne);
-             
+
+            while (resultado.next()) {
+                grupo.setCodigoGrupo(resultado.getInt("GRUPO_ID".trim()));
+                int codigoAsignatura = resultado.getInt("GRUPO_ASIGNTURA".trim());
+                Asignatura asig = controlAsig.buscarAsignatura(codigoAsignatura, nivel);
+                grupo.setC(asig);
+                int codigoEspacio = resultado.getInt("GRUPO_ESPACIOFISICO".trim());
+                EspacioFisico esp = controlEspacio.buscaEspacioFisico(codigoEspacio);
+                grupo.setCodigoEspacioFisico(esp);
+                int codigoDoccente = resultado.getInt("GRUPO_DOCENTE".trim());
+                Docente docne = controlDocen.buscarEsCodigo(codigoEspacio);
+                grupo.setDocenteCodigo(docne);
+
                 consulta.executeUpdate();
-         }
+            }
         } catch (Exception e) {
             res = "ERROR";
             c.desconectar();
         }
-            return "GRUPO CREADO";
-    } 
-    public String buscarGrupo (Grupo grupo, int codigo){
-         String res = "";
+        return "GRUPO CREADO";
+    }
+
+    public String buscarGrupo(Grupo grupo, int codigo) {
+        String res = "";
         String sql = "UPDATE GRUPO"
-                + " SET GRUPO_ID = " + " ' " + grupo.getCodigoGrupo()+ " ' " + ","
-                + "  GRUPO_ASIGNTURA =" + " ' " + grupo.getC()+ " ' " + ","
-                + "  GRUPO_ESPACIOFISICO =  " + " ' " + grupo.getCodigoEspacioFisico()+ " ' " + ","
-                + "  GRUPO_DOCENTE = " + " ' " + grupo.getDocenteCodigo()+ " ' "
+                + " SET GRUPO_ID = " + " ' " + grupo.getCodigoGrupo() + " ' " + ","
+                + "  GRUPO_ASIGNTURA =" + " ' " + grupo.getC() + " ' " + ","
+                + "  GRUPO_ESPACIOFISICO =  " + " ' " + grupo.getCodigoEspacioFisico() + " ' " + ","
+                + "  GRUPO_DOCENTE = " + " ' " + grupo.getDocenteCodigo() + " ' "
                 + "WHERE GRUPO_ID =" + " ' " + codigo + " ' ";
         try {
 
@@ -79,7 +82,7 @@ public class ControladorGrupo {
         return res;
     }
 
- public String eliminarGrupo(int codigo) {
+    public String eliminarGrupo(int codigo) {
         String res = "";
         String sql = "DELETE FROM GRUPO"
                 + " WHERE GRUPO_ID = " + "'" + codigo + "'";
@@ -95,7 +98,7 @@ public class ControladorGrupo {
 
         return res;
     }
- 
+
     public List<Grupo> listarGrupo(ControladorAsignatura controlAsig, ControladorDocente controlDocen, ControladorEspacioFisico controlEspacio, ControladorNivelAsignatura nivel) {
         List<Grupo> grupoList = new ArrayList<>();
         String sql = "SELECT GRUPO_ID,"
@@ -111,15 +114,15 @@ public class ControladorGrupo {
                 Grupo grupo = new Grupo();
                 grupo.setCodigoGrupo(resultado.getInt("ASIGNATURA_ID".trim()));
                 int codigoAsignatura = resultado.getInt("GRUPO_ASIGNTURA".trim());
-             Asignatura asig = controlAsig.buscarAsignatura(codigoAsignatura,nivel);
-             grupo.setC(asig);
-             int codigoEspacio = resultado.getInt("GRUPO_ESPACIOFISICO".trim());
-             EspacioFisico esp = controlEspacio.buscaEspacioFisico(codigoEspacio);
-             grupo.setCodigoEspacioFisico(esp);
-             int codigoDoccente = resultado.getInt("GRUPO_DOCENTE".trim());
-             Docente docne = controlDocen.buscarEsCodigo(codigoEspacio);
-             grupo.setDocenteCodigo(docne);
-             
+                Asignatura asig = controlAsig.buscarAsignatura(codigoAsignatura, nivel);
+                grupo.setC(asig);
+                int codigoEspacio = resultado.getInt("GRUPO_ESPACIOFISICO".trim());
+                EspacioFisico esp = controlEspacio.buscaEspacioFisico(codigoEspacio);
+                grupo.setCodigoEspacioFisico(esp);
+                int codigoDoccente = resultado.getInt("GRUPO_DOCENTE".trim());
+                Docente docne = controlDocen.buscarEsCodigo(codigoEspacio);
+                grupo.setDocenteCodigo(docne);
+
                 grupoList.add(grupo);
             }
         } catch (Exception ex) {
@@ -129,6 +132,7 @@ public class ControladorGrupo {
         }
         return grupoList;
     }
+
     public Grupo buscarGrupo(int codigo) {
         Grupo grupo = new Grupo();
         return grupo;

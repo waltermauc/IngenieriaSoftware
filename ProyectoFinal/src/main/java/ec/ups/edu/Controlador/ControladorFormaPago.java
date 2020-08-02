8e8e28325458df84d72c0892fc81b2f18fe287fa
orphe <stevenpalta@gmail.com>
2020-08-02 13:01:11 -0500

    control

--- ProyectoFinal/src/main/java/ec/ups/edu/Controlador/ControladorFormaPago.java
+++ ProyectoFinal/src/main/java/ec/ups/edu/Controlador/ControladorFormaPago.java
@@ -7,6 +7,9 @@ package ec.ups.edu.Controlador;
 
 import ec.ups.edu.Modelo.FormadePago;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -39,7 +42,7 @@ public class ControladorFormaPago {
         return res;
      }
     public FormadePago buscarFormaPago (int codigo){
-       String resultad0 ="";
+       String resulta ="";
          FormadePago forma = new FormadePago() {
            @Override
            public int calcularComision() {
@@ -50,10 +53,84 @@ public class ControladorFormaPago {
            public int calcularTotal() {
                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
            }
-         }
-      
-       
+         };
+         
+       try {
+            String sql = "SELECT * FROM FORMADEPAGO "
+                    + "WHERE FORMAPAGO_ID = " + "'" + codigo + "'";
+            PreparedStatement consulta = c.conectado().prepareStatement(sql);
+            ResultSet resultado = consulta.executeQuery();
+
+            while (resultado.next()) {
+
+                forma.setCodigo(resultado.getInt("FORMAPAGO_ID".trim()));
+                forma.setValor(resultado.getInt("FORMAPAGO_VALOR".trim()));
+                forma.setComision(resultado.getInt("FORMAPAGO_COMISION".trim()));
+
+            }
+        } catch (Exception e) {
+            c.desconectar();
+            return null;
+        }
+         
+         
+        return forma;  
     }
-     
+    public String actualizarFormaPago (FormadePago forma, int codigoActualizar){
+        String resultado = "";
+        String sql = "UPDATE FORMAPAGO"
+                + " SET FORMAPAGO_ID = " + " ' " + forma.getCodigo()+ " ' " + ","
+                + "  FORMAPAGO_VALOR =" + " ' " + forma.getValor()+ " ' " + ","
+                + "  FORMAPAGO_COMISION =  " + " ' " + forma.getComision()+ " ' " + ","
+                + "WHERE FORMAPAGO_ID =" + " ' " + codigoActualizar + " ' ";
+        try {
+
+            PreparedStatement ps;
+            ps = c.conectado().prepareStatement(sql);
+            ps.executeUpdate();
+            resultado= "exito al actualizar";
+
+        } catch (Exception ex) {
+            c.desconectar();
+            resultado=  "ERROR ";
+            
+        }
+        return resultado;
+    } 
+    
+    public String elimiarFormaPago (int codigoEliminar){
+        String resultadoElimiar= "";
+        String sql = "DELETE FROM FORMAPAGO"
+                + " WHERE FORMAPAGO_ID = " + "'" + codigoEliminar + "'";
+        try {
+
+            PreparedStatement ps = c.conectado().prepareStatement(sql);
+            ps.executeUpdate();
+            resultadoElimiar = "FORMAPAGO ELIMINADO";
+        } catch (Exception ex) {
+            resultadoElimiar = " ERROR ";
+            c.desconectar();
+        }
+
+        return resultadoElimiar;
+    }
+   public List <FormadePago> listarFormaPago(){
+       List<FormadePago> listaForma = new ArrayList<>();
+       String sql = "SELECT GRUPO_ID,"
+                + "GRUPO_ASIGNTURA,"
+                + "GRUPO_ESPACIOFISICO, "
+                + "GRUPO_DOCENTE,"
+                + " FROM GRUPO";
+        try {
+            PreparedStatement consulta = c.conectado().prepareStatement(sql);
+            ResultSet resultado = consulta.executeQuery();
+        }catch (Exception ex) {
+            ex.printStackTrace();
+            c.desconectar();
+
+        }
+    return listaForma;   
+   }
+           
             
 }
